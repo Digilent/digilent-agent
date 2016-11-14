0d65d104a813fe9f28e4e12cf5a1d07825e1fcad
Sam Kristoff <sam.kristoff@digilent.com>
2016-11-14 19:28:10 +0000

    Update url parsing to include trailing slash added by silverneedle

--- src/httpServer/httpRouter.cpp
+++ src/httpServer/httpRouter.cpp
@@ -13,13 +13,22 @@ HttpRouter::HttpRouter(QObject* _parent, OsDevice **_activeDevice) : HttpRequest
 }
 
 void HttpRouter::service(HttpRequest& request, HttpResponse& response) {
-    QByteArray path=request.getPath();
-    qDebug("HttpRouter: path=%s", path.data());
+    QByteArray path = request.getPath();
+    QByteArray method = request.getMethod();
 
-    if (path=="/debug") {       
+    qDebug() << "HttpRouter: method = " << method << "path = ", path.data();
+
+    if(method == "OPTIONS"){
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        response.write("Options Response....", true);
+    }
+
+    else if (path=="/debug" || path=="/debug/") {
         qDebug("Routing To Debug Controller");
         DebugController(this).service(request, response);
-    } else if (path=="/proxy") {
+    } else if (path=="/proxy" || path=="/proxy/") {
 
         //Setup device request slot and initiate device call
         connect(*activeDevice, SIGNAL(execCommandComplete(QString)), this, SLOT(onComplete(QString)));
@@ -32,7 +41,13 @@ void HttpRouter::service(HttpRequest& request, HttpResponse& response) {
         loop.exec();
 
         //Return device call response to original requester
-        response.write(reply.toUtf8());
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "close");
+        response.setHeader("Content-Type", "application/json");
+        response.setStatus(200, "OK");
+        //response.setHeader("Content-Length", "1416");
+        response.write(reply.toUtf8(), true);
     }
     else{
         qDebug("Routing To Static Controller");
