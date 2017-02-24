791c837c232b6fa6711bbe6abf980eac2024c64c
Sam Kristoff <sam.kristoff@digilent.com>
2017-02-24 17:52:21 +0000

    Build Windows 0.3.2

--- src/core/agent.cpp
+++ src/core/agent.cpp
@@ -18,7 +18,7 @@ Agent::Agent(QObject *parent) : QObject(parent)
     this->httpCapable = true;
     this->majorVersion = 0;
     this->minorVersion = 3;
-    this->patchVersion = 1;
+    this->patchVersion = 2;
 
     this->firmwareUploadStatus = "idle";
 
