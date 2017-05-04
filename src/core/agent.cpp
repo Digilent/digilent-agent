20e65d1e6f3d514cee43c56021e41d7be19a6154
Sam Kristoff <sam.kristoff@digilent.com>
2017-05-04 17:51:08 +0100

    Fix issue preventing large chunks from being parsed correctly

--- src/core/agent.cpp
+++ src/core/agent.cpp
@@ -18,7 +18,7 @@ Agent::Agent(QObject *parent) : QObject(parent)
     this->httpCapable = true;
     this->majorVersion = 0;
     this->minorVersion = 3;
-    this->patchVersion = 33;
+    this->patchVersion = 34;
 
     this->firmwareUploadStatus = "idle";
 
