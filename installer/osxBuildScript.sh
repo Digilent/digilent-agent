c6da3602409c579a97c461e940f4d0a86dd2ee14
Digilent Software <software@digilent.com>
2017-07-12 08:54:56 -0700

    Update OSX build script

--- installer/osxBuildScript.sh
+++ installer/osxBuildScript.sh
@@ -1,9 +1,46 @@
 VERSION=$1
+BUILDTYPE=$2
+DATE=`date +%Y-%m-%d`
 
-#Copy build files into installer directory
-printf "Copying build to data directory...\n"
-rm -r ./packages/com.digilent.waveformsliveagent/data/*
-cp -r ../release/waveforms-live-agent.app	 packages/com.digilent.waveformsliveagent/data/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR=$DIR/working
+TARGET_APP=$BUILD_DIR"/Digilent Agent.app"
 
-printf "Building Installer...\n"
-binarycreator -c config/osxConfig.xml -p packages "waveformsliveagent-$VERSION"
+#Remove any old files
+printf "Cleaning build directory\n"
+rm -r -f "$BUILD_DIR/"
+
+#Create build directory if it does not exist
+if [ ! -d "$BUILD_DIR" ]; then
+  printf "Creating build directory\n"
+  mkdir -p $BUILD_DIR
+fi
+
+printf "Copying files to build directory\n"
+cp -r ../Release/digilent-agent.app "$TARGET_APP"
+ln -s /Users/Shared working/Shared
+mkdir -p working/digilent/digilent-agent/www
+cp -r waveforms-live-offline/ working/digilent/digilent-agent/www
+
+
+printf "Packaging Dependencies...\n"
+macdeployqt "$TARGET_APP" -codesign="Mac Developer: software@" -always-overwrite
+
+printf "Signing...\n"
+codesign --force --verify --verbose --sign "Mac Developer: software" "$TARGET_APP"
+find "$TARGET_APP" -name *.dylib | xargs -I $ codesign --force --verify --verbose --sign "Mac Developer: software" $
+find "$TARGET_APP" -name Qt* -type f | xargs -I $ codesign --force --verify --verbose --sign "Mac Developer: software" $
+
+printf "Creating .dmg...\n"
+./create-dmg/create-dmg.sh \
+--volname "Digilent Agent $VERSION" \
+--background ../images/digilent-agent-dmg-background.png \
+--window-size 640 430 \
+--icon-size 64 \
+--icon "Digilent\ Agent.app" 240 160 \
+--app-drop-link 400 160 \
+--icon "digilent" 240 300 \
+--icon "Shared" 400 300 \
+digilent-agent-$VERSION.dmg \
+working/ \
+\
